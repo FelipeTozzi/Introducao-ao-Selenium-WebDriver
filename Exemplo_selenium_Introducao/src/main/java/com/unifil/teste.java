62c149bd3cca9a4a5193bf0161a44d15a62e8ef3
Felipe Tozzi <felipetozzi@edu.unifil.br>
2025-03-15 23:59:27 -0300

    correcaao

--- Exemplo_selenium_Introducao/src/main/java/com/unifil/teste.java
+++ Exemplo_selenium_Introducao/src/main/java/com/unifil/teste.java
@@ -3,35 +3,30 @@ package com.unifil;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 import java.time.Duration;
 
 public class teste {
     public static void main(String[] args) {
-        // Configura o caminho do GeckoDriver (Firefox)
         System.setProperty("webdriver.edge.driver", "C:\\Selenium\\msedgedriver.exe");
 
-        // Inicializa o WebDriver para Firefox
-        WebDriver driver = new FirefoxDriver();
+        WebDriver driver = new EdgeDriver();
 
         try {
-            // Acessa o portal da UniFil
+
             driver.get("https://portal.unifil.br/");
 
-            // Maximiza a janela do navegador
             driver.manage().window().maximize();
 
-            // Tempo de espera para carregar os elementos
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-            // Localiza o campo de matrícula e insere os dados
             WebElement matriculaInput = driver.findElement(By.id("CPF"));
             matriculaInput.sendKeys("6371829");
 
-            // Localiza o botão "Acessar" e clica nele
             WebElement acessarButton = driver.findElement(By.id("Acessar"));
             acessarButton.click();
-            
+
             Thread.sleep(5000);
         } catch (Exception e) {
             e.printStackTrace();
